0b7c3d84f5ca49372db75f605117e2f2388024db
M. Mostafa Farzan <m2_farzan@yahoo.com>
2022-01-07 17:53:36 +0330

    Downgrade pyqt5

--- .github/workflows/test.bash
+++ .github/workflows/test.bash
@@ -13,5 +13,8 @@ cat .SRCINFO | grep -oP "depends\ \=\ \K.+" | xargs sudo -u ${AUR_USER} yay -S -
 # Temporary fix (waiting for https://github.com/acxz/pkgbuilds/issues/161)
 pacman -S python-pip --noconfirm --noprogressbar
 pip3 install colcon-core==0.7.0
+# Temporary fix (https://github.com/m2-farzan/ros2-galactic-PKGBUILD/issues/9)
+wget -O python-pyqt5-5.15.5-1-x86_64.pkg.tar.zst https://archive.archlinux.org/packages/p/python-pyqt5/python-pyqt5-5.15.5-1-x86_64.pkg.tar.zst
+pacman -U python-pyqt5-5.15.5-1-x86_64.pkg.tar.zst --noconfirm --noprogressbar
 
 sudo -u ${AUR_USER} makepkg
