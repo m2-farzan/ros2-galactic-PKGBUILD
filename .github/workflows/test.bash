ccb716e1581cf1f4666e3edbb8ca99811cbeb2c7
M. Mostafa Farzan <m2_farzan@yahoo.com>
2022-01-07 15:15:47 +0330

    Do git-config as AUR_USER

--- .github/workflows/test.bash
+++ .github/workflows/test.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 cd /ros2-galactic-pkgbuild
 AUR_USER=ab
 chown -R ${AUR_USER}:${AUR_USER} .
+
 sudo pacman -Syyu --noconfirm --noprogressbar
-git config --global user.name "Test User"
-git config --global user.email "test@example.com"
+sudo -u ${AUR_USER} git config --global user.name "Test User"
+sudo -u ${AUR_USER} git config --global user.email "test@example.com"
 cat .SRCINFO | grep -oP "depends\ \=\ \K.+" | xargs sudo -u ${AUR_USER} yay -S --noconfirm --noprogressbar --needed
 sudo -u ${AUR_USER} makepkg
