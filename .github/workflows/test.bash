25a8acae977e3f13a398c5318fbe595bc7105c62
M. Mostafa Farzan <m2_farzan@yahoo.com>
2022-01-07 14:52:12 +0330

    Try un-sudo with AUR_USER

--- .github/workflows/test.bash
+++ .github/workflows/test.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd /ros2-galactic-pkgbuild
+AUR_USER=ab
 sudo pacman -Syyu --noconfirm --noprogressbar
 git config --global user.name "Test User"
 git config --global user.email "test@example.com"
-cat .SRCINFO | grep -oP "depends\ \=\ \K.+" | xargs yay -S --noconfirm --noprogressbar --needed
+cat .SRCINFO | grep -oP "depends\ \=\ \K.+" | xargs sudo -u ${AUR_USER} yay -S --noconfirm --noprogressbar --needed
 makepkg
