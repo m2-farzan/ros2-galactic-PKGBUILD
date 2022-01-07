683c94b06e873b1f51867287ce4c01f7dda45fec
M. Mostafa Farzan <m2_farzan@yahoo.com>
2022-01-07 14:38:18 +0330

    Drop docker cache, move instructions to shell script

---
+++ .github/workflows/test.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo pacman -Syyu --noconfirm --noprogressbar
+git config --global user.name "Test User"
+git config --global user.email "test@example.com"
+cat .SRCINFO | grep -oP "depends\ \=\ \K.+" | xargs sudo yay -S --noconfirm --noprogressbar --needed
+makepkg
