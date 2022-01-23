ba2f3aeec45d60e2f4236857c0b5f3796fee5537
Mohammad Farzan <m2_farzan@yahoo.com>
2022-01-23 13:33:18 +0330

    CI update
    - Use manual trigger
    - Status badge set to extended branch
    - manual colcon update removed from test script

--- .github/workflows/test.bash
+++ .github/workflows/test.bash
@@ -15,7 +15,4 @@ sudo -u ${AUR_USER} git config --global user.name "Test User"
 sudo -u ${AUR_USER} git config --global user.email "test@example.com"
 cat .SRCINFO | grep -oP "depends\ \=\ \K.+" | xargs sudo -u ${AUR_USER} yay -S --noconfirm --noprogressbar --needed
 
-# Temporary fix (waiting for https://github.com/acxz/pkgbuilds/issues/161)
-pip3 install colcon-core==0.7.0
-
 sudo -u ${AUR_USER} makepkg
